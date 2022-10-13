a6085d41cf72dfb5df7ba16aa0a3097c9bd2d09a
pndhkm <72283525+pndhkm@users.noreply.github.com>
2022-10-13 09:00:03 +0700

    Create cpanel-versions.sh

---
+++ cpanel-versions.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+jb_not_found() {
+  if type "$1" >/dev/null 2>&1; then # if given a valid command
+    jb=$(jetbackup --version | awk 'NR==1 {print $2}')
+  else
+    jb=$(jetbackup5 --version | awk 'NR==1 {print $2}')
+  fi
+}
+
+
+function versions() {
+        cp=$(/usr/local/cpanel/cpanel -V)
+        os=$(grep "PRETTY" /etc/os-release |  awk -F'=' '{print $2}' | tr -d '"')
+        ls=$(/usr/local/lsws/bin/lshttpd -v | cut -d "/" -f2)
+        db=$(mysql --version | awk '{print $1"-"$5}' | cut -d ',' -f1)
+        php=$(php -v | awk 'NR==1 {print $2}')
+        jb=$(jetbackup --version | awk 'NR==1 {print $2}')
+        echo "Operating Systems|: $os"
+        echo "Hostname|: $HOSTNAME"
+        echo "WHM/Cpanel|: $cp"
+        echo "Litespeed|: $ls"
+        echo "Database|: $db"
+        echo "PHP|: $php"
+        echo "Jetbackup|: $(jb_not_found)"
+}
+
+versions | column -t -s'|'
