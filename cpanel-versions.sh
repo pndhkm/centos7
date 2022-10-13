73437d95e4387ca88b6bd83d71e91b1abbffd1bb
pndhkm <72283525+pndhkm@users.noreply.github.com>
2022-10-13 08:17:10 +0700

    Create cpanel-versions.sh

---
+++ cpanel-versions.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/bash
+
+function versions() {
+        cp=$(/usr/local/cpanel/cpanel -V)
+        os=$(grep "PRETTY" /etc/os-release |  awk -F'=' '{print $2}' | tr -d '"')
+        ls=$(/usr/local/lsws/bin/lshttpd -v | cut -d "/" -f2)
+        db=$(mysql --version | awk '{print $1"-"$5}' | cut -d ',' -f1)
+        php=$(php -v | awk 'NR==1 {print $2}')
+        jb=$(jetbackup --version | awk 'NR==1 {print $2}')
+        echo "Operating Systems|: $os"
+        echo "Hostname|: $HOSTNAME"
+        echo "WHM/Cpanel|: $cp"
+        echo "Litespeed|: $ls"
+        echo "Database|: $db"
+        echo "PHP|: $php"
+        echo "Jetbackup|: $jb"
+}
+
+versions | column -t -s'|'
